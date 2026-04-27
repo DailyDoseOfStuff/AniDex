c3ad2169d0f7417cf1f225437416fef415e2468c
DailyDoseOfStuff <allenliu@outlook.com>
2026-04-27 02:55:16 -0500

    fix: deploy as static assets to avoid wrangler vite config injection error

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite';
 
-import { cloudflare } from "@cloudflare/vite-plugin";
-
 export default defineConfig({
   root: '.',
   publicDir: 'public',
-  plugins: [cloudflare()],
+  plugins: [],
   server: {
     port: 3000,
     open: true,
